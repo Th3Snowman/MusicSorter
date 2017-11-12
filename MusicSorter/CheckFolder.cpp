9899b968a675730a3eb35c8341cd8e8756bd8126
The_Snowman <aidanrbwood@gmail.com>
2017-11-11 21:18:00 -0500

    Added base files to repo

---
+++ MusicSorter/CheckFolder.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <boost/filesystem.hpp>
+
+//check if inputted path is a folder, and then populate an array of its items using a directory_iterator
